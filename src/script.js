ef0b48a1c55ef112e40d01b3804977b38f75a758
ByOnez <enzolupki.dev@gmail.com>
2023-04-14 20:09:50 -0300

    finished css

--- src/script.js
+++ src/script.js
@@ -1,44 +1,39 @@
-const apikey = ("6e887ca436f6731c6354e94495c86a87");
-const city = "Linhares"
+const apikey = "6e887ca436f6731c6354e94495c86a87";
 
-const weatherSvg = document.getElementById("weatherSvg")
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}`)
-  .then(response => response.json())
-  .then(data => {
+document.addEventListener("DOMContentLoaded", function() {
+  let searchCity = document.getElementById("searchCity");
+  searchCity.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      let city = searchCity.value;
     
-    const kelvin = data.main.temp
-    const celsius = kelvin - 273.15
-    const weather = data.weather[0].main
-    const humidity = data.main.humidity
-    const wind = data.wind.speed  
-    const windKmh = wind * 3.6
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}`)
+        .then(response => response.json())
+        .then(data => {
+          const kelvin = data.main.temp;
+          const celsius = kelvin - 273.15;
+          const weather = data.weather[0].main;
+          const humidity = data.main.humidity;
+          const wind = data.wind.speed;
+          const windKmh = wind * 3.6;
 
-    let cityName = document.getElementById("city")
-    let temperatureValue = document.getElementById("temperature")
-    let weatherDescription = document.getElementById("weather")
-    let humidityValue = document.getElementById("humidity")
-    let windSpeed = document.getElementById("wind")
+          let cityName = document.getElementById("city");
+          let temperatureValue = document.getElementById("temperature");
+          let weatherDescription = document.getElementById("weather");
+          let humidityValue = document.getElementById("humidity");
+          let windSpeed = document.getElementById("wind");
 
-    cityName.innerHTML = city
-    temperatureValue.innerHTML = (`${celsius.toFixed(0)}°`)
-    weatherDescription.innerHTML = (`${weather}`)
-    humidityValue.innerHTML = (`Humidade ${humidity}%`)
-    windSpeed.innerHTML = (`Vento ${windKmh.toFixed(0)}Km/h`)
-    
-    console.log(`Forma que vem a ${weather}`)
-    
-    if (weather === "Clouds") {
-      weatherSvg = "cloudy.svg"
-    } else {
-      weatherSvg = "clear-day"
+          cityName.innerHTML = city.toUpperCase();
+          temperatureValue.innerHTML = `${celsius.toFixed(0)}°`;
+          weatherDescription.innerHTML = `${weather}`;
+          humidityValue.innerHTML = `Humidity ${humidity}%`;
+          windSpeed.innerHTML = `Wind ${windKmh.toFixed(0)} Km/h`;
+
+          if(city === "Clouds") {
+            
+          }
+        })
+        .catch(error => console.error(error));
     }
-  
-    
-})
-  .catch(error => console.error(error));
-
-  
-   
-  
-  
+  });
+});
