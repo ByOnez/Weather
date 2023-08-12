719791a33a5a6a657448f2d160d0b66dafce9f16
ByOnez <118693852+ByOnez@users.noreply.github.com>
2023-08-12 01:09:20 -0300

    Script updates

--- src/script.js
+++ src/script.js
@@ -1,32 +1,43 @@
-const apikey = ""
+const apikey = "8c10597313775f5b4020538716c600ff";
+let defaultCity = "São Paulo"; 
 
 document.addEventListener("DOMContentLoaded", function() {
-  let searchCity = document.getElementById("searchCity")
-  searchCity.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault()
-      let city = searchCity.value
+  let searchCity = document.getElementById("searchCity");
+  let city = defaultCity; 
+
+  function updateInterface(data) {
+    const { main: { temp: kelvin, humidity }, weather: [{ main: weather }], wind: { speed: wind } } = data;
+    const celsius = kelvin - 273.15;
+    const windKmh = wind * 3.6;
+
+    let cityName = document.getElementById("city");
+    let temperatureValue = document.getElementById("temperature");
+    let weatherDescription = document.getElementById("weather");
+    let humidityValue = document.getElementById("humidity");
+    let windSpeed = document.getElementById("wind");
+
+    cityName.innerHTML = city.toUpperCase();
+    temperatureValue.innerHTML = `${celsius.toFixed(0)}°C`;
+    weatherDescription.innerHTML = `${weather}`;
+    humidityValue.innerHTML = `Humidade ${humidity}%`;
+    windSpeed.innerHTML = `Vento ${windKmh.toFixed(0)} Km/h`;
+  }
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}`)
+    .then(response => response.json())
+    .then(data => {
+      updateInterface(data);
+    })
+    .catch(error => console.error(error));
+
+  searchCity.addEventListener("input", function() {
+    city = searchCity.value.trim();
     
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}`)
-        .then(response => response.json())
-        .then(data => {
-          const { main: { temp: kelvin, humidity }, weather: [{ main: weather }], wind: { speed: wind } } = data
-          const celsius = kelvin - 273.15
-          const windKmh = wind * 3.6
-
-          let cityName = document.getElementById("city")
-          let temperatureValue = document.getElementById("temperature")
-          let weatherDescription = document.getElementById("weather")
-          let humidityValue = document.getElementById("humidity")
-          let windSpeed = document.getElementById("wind")
-
-          cityName.innerHTML = city.toUpperCase()
-          temperatureValue.innerHTML = `${celsius.toFixed(0)}°C`
-          weatherDescription.innerHTML = `${weather}`
-          humidityValue.innerHTML = `Humidity ${humidity}%`
-          windSpeed.innerHTML = `Wind ${windKmh.toFixed(0)} Km/h`
-        })
-        .catch(error => console.error(error))
-    }
-  })
-})
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&appid=${apikey}`)
+      .then(response => response.json())
+      .then(data => {
+        updateInterface(data);
+      })
+      .catch(error => console.error(error));
+  });
+});
